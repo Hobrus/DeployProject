caaf2c912e2c1bdfa4777dadbdd54d830d924ce9
Hobrus <bhobrus@gmail.com>
2023-07-20 19:24:40 +0300

    Production ready

--- Deploy/Deploy/settings.py
+++ Deploy/Deploy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&%8vhpi=vne7bv!7orpiq0rds4y55*z+r@lkh_rvp+ak7i1o3+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
